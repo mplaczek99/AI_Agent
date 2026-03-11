c01f8efeae687b1b589ffab6071c5a0f4e8690a2
mplaczek99 <mplaczek99@gmail.com>
2026-03-11 16:44:56 -0500

    Created Messages to act as a history of messages
    It kinda works, just not needs a for loop (maybe) to append to messages

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -18,10 +19,11 @@ parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 args = parser.parse_args()
 
+# Start a list of messages to send
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
 # Prompt the model
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents=args.user_prompt
-)
+response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
 
 # Makes sure the usage_metadata exists
 if response.usage_metadata is None:
