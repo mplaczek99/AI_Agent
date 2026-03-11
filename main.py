7b9fde9b9da0a0acece01174053f81d0afc47adc
mplaczek99 <mplaczek99@gmail.com>
2026-03-10 21:37:39 -0500

    Used argsparse instead of hard-coding the prompt
    Can now type the prompt in with the command, no need to hard-code the
    prompt with main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 from dotenv import load_dotenv
 from google import genai
@@ -12,20 +13,26 @@ if api_key is None:
 # Load a model from the api_key
 client = genai.Client(api_key=api_key)
 
+# Check for arguments in the main.py command
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
 # Prompt the model
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents=args.user_prompt
+)
 
 # Makes sure the usage_metadata exists
 if response.usage_metadata is None:
-    raise RuntimeError("The API response appears malformed...")
+    raise RuntimeError("The API response appears malformed, it probably failed!")
 
 # Create variables of the prompt and response
 prompt_count = response.usage_metadata.prompt_token_count
 response_count = response.usage_metadata.candidates_token_count
 
 # Print those variables
-print(f"User prompt: {prompt}")
+print(f"User prompt: {args.user_prompt}")
 print(f"Prompt tokens: {prompt_count}")
 print(f"Response tokens: {response_count}")
 print(f"Response:\n{response.text}")
