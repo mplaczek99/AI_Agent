3d8ccafa3df22e40427cc1512461ac9e0bdf8c56
mplaczek99 <mplaczek99@gmail.com>
2026-04-14 18:59:52 -0500

    Added logic so I can send a prompt to Gemini

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 
 def main():
@@ -36,6 +37,10 @@ def generate_content(client, messages, user_prompt, verbose):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt,
+            temperature=0,
+        ),
     )
 
     # Verify the response contains usage metadata
